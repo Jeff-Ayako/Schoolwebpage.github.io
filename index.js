abc78ac46e85e2ad5adf3551a5bd2d4594bd34cd
Jeff Ayako, HSC <Jeffayako1@gmail.com>
2022-03-03 01:00:36 +0300

    updates

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const body= document.querySelector('body');
 const darkBtn=document.querySelector('.dark-theme');
+const menuicon=document.querySelector('.show-content');
+const courseContent=document.querySelector('.Course-content');
+
+menuicon.addEventListener('click',()=>{
+    menuicon.classList.toggle('courseConten');
+});
 
 darkBtn.addEventListener('click', ()=>{
     body.classList.toggle('dark-theme');
